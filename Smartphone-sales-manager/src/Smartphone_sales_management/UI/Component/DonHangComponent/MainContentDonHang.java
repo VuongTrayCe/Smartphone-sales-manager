d3f2872b87b9199e14bbac5842925de7291bed05
diplamtuan <85506794+diplamtuan@users.noreply.github.com>
2022-10-29 21:47:47 +0700

    update

--- Smartphone-sales-manager/src/Smartphone_sales_management/UI/Component/DonHangComponent/MainContentDonHang.java
+++ Smartphone-sales-manager/src/Smartphone_sales_management/UI/Component/DonHangComponent/MainContentDonHang.java
@@ -36,6 +36,7 @@ public class MainContentDonHang extends javax.swing.JPanel {
                 detailDonHangPanel.add(detailDH);
                 repaint();
                 detailDH.validate();
+                System.out.println(index);
                 return index;
             }
         });
@@ -72,8 +73,8 @@ public class MainContentDonHang extends javax.swing.JPanel {
         jComboBox2 = new javax.swing.JComboBox<>();
         jScrollPane1 = new javax.swing.JScrollPane();
         detailDonHangPanel = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(102, 255, 102));
 
@@ -126,12 +127,6 @@ public class MainContentDonHang extends javax.swing.JPanel {
             .addGap(0, 435, Short.MAX_VALUE)
         );
 
-        jButton1.setBackground(new java.awt.Color(255, 0, 51));
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Smartphone_sales_management/UI/Icon/Delete.png"))); // NOI18N
-        jButton1.setText("Xóa đơn hàng");
-
         jButton2.setBackground(new java.awt.Color(255, 0, 51));
         jButton2.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jButton2.setForeground(new java.awt.Color(255, 255, 255));
@@ -140,6 +135,17 @@ public class MainContentDonHang extends javax.swing.JPanel {
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setBackground(new java.awt.Color(255, 0, 51));
+        jButton3.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(255, 255, 255));
+        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Smartphone_sales_management/UI/Icon/Very-Basic-Print-icon.png"))); // NOI18N
+        jButton3.setText("In hóa đơn");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
             }
         });
 
@@ -153,14 +159,14 @@ public class MainContentDonHang extends javax.swing.JPanel {
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 627, Short.MAX_VALUE)
                         .addGap(10, 10, 10)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(detailDonHangPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 368, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(detailDonHangPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 368, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addContainerGap())
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addGap(0, 0, 0)
+                                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(26, 26, 26)
                                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jLabel8, javax.swing.GroupLayout.DEFAULT_SIZE, 194, Short.MAX_VALUE)
@@ -182,9 +188,9 @@ public class MainContentDonHang extends javax.swing.JPanel {
                             .addComponent(txtTimkiem, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 435, Short.MAX_VALUE)
@@ -234,11 +240,15 @@ public class MainContentDonHang extends javax.swing.JPanel {
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
 
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel detailDonHangPanel;
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
--- Smartphone-sales-manager/src/Smartphone_sales_management/UI/Component/DonHangComponent/TableDetailDH.java
+++ Smartphone-sales-manager/src/Smartphone_sales_management/UI/Component/DonHangComponent/TableDetailDH.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Vector;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
 
 /**
  *
@@ -27,9 +28,9 @@ public class TableDetailDH extends javax.swing.JPanel {
         initComponents();
         selectedIndex = index;
         if (selectedIndex != -1) {
-            
+
             data = qldh_BUS.layDanhSachChiTietDonHang(this.selectedIndex);
-            
+
             DefaultComboBoxModel modelTenSP = new DefaultComboBoxModel();
 //            Tao bien arrSp de luu tenSP roi add vao modelTenSP
             for (Object object : data) {
@@ -47,11 +48,18 @@ public class TableDetailDH extends javax.swing.JPanel {
         data = qldh_BUS.layDanhSachChiTietDonHang(this.selectedIndex);
         Vector dataDetail = new Vector();
         dataDetail = (Vector) data.get(jComboBox1.getSelectedIndex());
-        lbcName2.setText(dataDetail.get(0).toString());
-        lbcName2.setForeground(Color.red);
+        trangthailb.setText(dataDetail.get(0).toString());
+        trangthailb.setForeground(Color.red);
+        if (dataDetail.get(0).toString().equals("Đã xử lí") || dataDetail.get(0).toString().equals("Hủy đơn")) {
+            confirmBtn.setVisible(false);
+            deleteBtn.setVisible(false);
+        } else if (dataDetail.get(0).toString().equals("Chưa xử lí")) {
+            confirmBtn.setVisible(true);
+            deleteBtn.setVisible(true);
+        }
         lbcLoai.setText(dataDetail.get(2).toString());
         lbcSL1.setText(dataDetail.get(3).toString());
-        lbcKhuyenMai.setText(dataDetail.get(4).toString()+"%");
+        lbcKhuyenMai.setText(dataDetail.get(4).toString() + "%");
         lbcBHKT.setText(dataDetail.get(5).toString());
         lbcGia1.setText(dataDetail.get(6).toString());
         lbcgiasaukm.setText(dataDetail.get(7).toString());
@@ -73,13 +81,15 @@ public class TableDetailDH extends javax.swing.JPanel {
         lbcKhuyenMai = new javax.swing.JLabel();
         lbcGia1 = new javax.swing.JLabel();
         lbTrangThai = new javax.swing.JLabel();
-        lbcName2 = new javax.swing.JLabel();
+        trangthailb = new javax.swing.JLabel();
         jComboBox1 = new javax.swing.JComboBox<>();
         lbBHKT = new javax.swing.JLabel();
         lbcBHKT = new javax.swing.JLabel();
         lbcLoai = new javax.swing.JLabel();
         lbGia1 = new javax.swing.JLabel();
         lbcgiasaukm = new javax.swing.JLabel();
+        confirmBtn = new javax.swing.JButton();
+        deleteBtn = new javax.swing.JButton();
         jPanel4 = new javax.swing.JPanel();
         jPanel6 = new javax.swing.JPanel();
         lbImage = new javax.swing.JLabel();
@@ -120,7 +130,7 @@ public class TableDetailDH extends javax.swing.JPanel {
         lbTrangThai.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         lbTrangThai.setText("Trạng Thái:");
 
-        lbcName2.setText("jLabel10");
+        trangthailb.setText("jLabel10");
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         jComboBox1.addActionListener(new java.awt.event.ActionListener() {
@@ -140,6 +150,18 @@ public class TableDetailDH extends javax.swing.JPanel {
         lbGia1.setText("Giá sau khuyến mãi :");
 
         lbcgiasaukm.setText("jLabel15");
+
+        confirmBtn.setBackground(new java.awt.Color(255, 0, 51));
+        confirmBtn.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        confirmBtn.setForeground(new java.awt.Color(255, 255, 255));
+        confirmBtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Smartphone_sales_management/UI/Icon/check1.png"))); // NOI18N
+        confirmBtn.setText("Xác nhận đơn hàng");
+
+        deleteBtn.setBackground(new java.awt.Color(255, 0, 51));
+        deleteBtn.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        deleteBtn.setForeground(new java.awt.Color(255, 255, 255));
+        deleteBtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Smartphone_sales_management/UI/Icon/Close-icon.png"))); // NOI18N
+        deleteBtn.setText("Hủy đơn hàng");
 
         javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
         jPanel5.setLayout(jPanel5Layout);
@@ -166,9 +188,9 @@ public class TableDetailDH extends javax.swing.JPanel {
                             .addComponent(lbKhuyenMai, javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lbBHKT, javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lbGia, javax.swing.GroupLayout.Alignment.LEADING))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 138, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 146, Short.MAX_VALUE)
                         .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lbcName2, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(trangthailb, javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(lbcGia1, javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(lbcBHKT, javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(lbcKhuyenMai, javax.swing.GroupLayout.Alignment.TRAILING)
@@ -178,7 +200,12 @@ public class TableDetailDH extends javax.swing.JPanel {
                     .addGroup(jPanel5Layout.createSequentialGroup()
                         .addComponent(lbGia1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(lbcgiasaukm)))
+                        .addComponent(lbcgiasaukm))
+                    .addGroup(jPanel5Layout.createSequentialGroup()
+                        .addComponent(confirmBtn)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(deleteBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(6, 6, 6)))
                 .addContainerGap())
         );
         jPanel5Layout.setVerticalGroup(
@@ -187,7 +214,7 @@ public class TableDetailDH extends javax.swing.JPanel {
                 .addGap(10, 10, 10)
                 .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lbTrangThai, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lbcName2))
+                    .addComponent(trangthailb))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lbTenSanPham)
@@ -216,7 +243,11 @@ public class TableDetailDH extends javax.swing.JPanel {
                 .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lbGia1)
                     .addComponent(lbcgiasaukm))
-                .addContainerGap(125, Short.MAX_VALUE))
+                .addGap(34, 34, 34)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(confirmBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(deleteBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(42, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -225,9 +256,9 @@ public class TableDetailDH extends javax.swing.JPanel {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGap(90, 90, 90)
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 190, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(74, 74, 74))
-            .addComponent(jPanel5, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 354, Short.MAX_VALUE)
+            .addComponent(jPanel5, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 362, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -248,7 +279,7 @@ public class TableDetailDH extends javax.swing.JPanel {
             jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel6Layout.createSequentialGroup()
                 .addGap(10, 10, 10)
-                .addComponent(lbImage, javax.swing.GroupLayout.DEFAULT_SIZE, 8, Short.MAX_VALUE)
+                .addComponent(lbImage, javax.swing.GroupLayout.DEFAULT_SIZE, 16, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel6Layout.setVerticalGroup(
@@ -296,6 +327,8 @@ public class TableDetailDH extends javax.swing.JPanel {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton confirmBtn;
+    private javax.swing.JButton deleteBtn;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
@@ -315,8 +348,8 @@ public class TableDetailDH extends javax.swing.JPanel {
     private javax.swing.JLabel lbcGia1;
     private javax.swing.JLabel lbcKhuyenMai;
     private javax.swing.JLabel lbcLoai;
-    private javax.swing.JLabel lbcName2;
     private javax.swing.JLabel lbcSL1;
     private javax.swing.JLabel lbcgiasaukm;
+    private javax.swing.JLabel trangthailb;
     // End of variables declaration//GEN-END:variables
 }
